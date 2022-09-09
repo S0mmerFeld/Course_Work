a417c948c10385ff3d5190b03147664a7e519de9
S0mmerfeld <olegyevseienko@gmail.com>
2022-09-09 13:44:12 +0300

    Finish Task number 1

--- Course_Work/Form1.Designer.cs
+++ Course_Work/Form1.Designer.cs
@@ -30,12 +30,69 @@ namespace Course_Work
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bStart.Location = new System.Drawing.Point(191, 268);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(254, 53);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(96, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result: ...";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Enabled = false;
+            this.tbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.Location = new System.Drawing.Point(103, 169);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(443, 38);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(651, 370);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Timer tClock;
     }
 }
 
--- Course_Work/Form1.cs
+++ Course_Work/Form1.cs
@@ -12,9 +12,66 @@ namespace Course_Work
 {
     public partial class Form1 : Form
     {
+        private bool Mode; // Режим дозволу / заборони введення даних
+
         public Form1()
         {
+            this.Mode = true;
             InitializeComponent();
+        }
+
+
+
+
+
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Attention. 25 second has passed. Do something!", "Warning");
+            tClock.Start();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if(Mode == true)
+            {
+                bStart.Text = "Стоп";
+                tClock.Start();
+                tbInput.Enabled = true;
+                Mode = false;
+                tbInput.Focus();
+            }
+            else
+            {
+                bStart.Text = "Старт";
+                tClock.Stop();
+                tbInput.Enabled = false;
+                Mode = true;
+            }
+
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+
+            if ((e.KeyChar >= 'a' && e.KeyChar <= 'z') | (e.KeyChar >= 'A' && e.KeyChar <= 'Z') | (e.KeyChar == (char) 8) | (e.KeyChar == ' '))
+            {
+               
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Please enter only alphabetic chars", "ERROR");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
+
+
+            
         }
     }
 }
